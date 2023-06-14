f4f047673b0e1bd481c8b2aa7185689fc635a067
buni_buni <720427ar@gmail.com>
2023-06-14 22:29:50 +0530

    fe v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CreateTodoComponent } from './create-todo/create-todo.component'
-// const routes: Routes = [];
-
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule]
-// })
-
+import { CreateTodoComponent } from './create-todo/create-todo.component';
+import { FetchTodoComponent } from './fetch-todo/fetch-todo.component'
 
 const routes: Routes = [
   { path: 'createList', component: CreateTodoComponent },
- // { path: 'expenses/detail/:id', component: ExpenseEntryComponent },
+  { path: 'fetchList', component: FetchTodoComponent },
   { path: '', redirectTo: 'expenses', pathMatch: 'full' }];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/fetch-todo/fetch-todo.component.ts
+++ src/app/fetch-todo/fetch-todo.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fetch-todo.component.scss']
 })
 export class FetchTodoComponent implements OnInit {
-
+  ItemsArray:any;
+  showTable: boolean =false;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  getTodoList () {
+    this.showTable= true;
+    this.ItemsArray= [{
+      "taskName": "s",
+      "desc": "s",
+      "startDate": "2023-06-14",
+      "endDate": "2023-06-29",
+      "email": "s@s.com",
+      "efforts": "3"
+  },
+  {
+    "taskName": "ss",
+    "desc": "as",
+    "startDate": "2023-06-24",
+    "endDate": "2023-06-30",
+    "email": "22@s.com",
+    "efforts": "13"
+}
+]
+  }
 }
